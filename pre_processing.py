0c187c8071b4624ca6a03bcb4bc028db0ac6c0cc
prajual <prajualpillai@gmail.com>
2022-08-15 07:26:13 +0530

    added impute function to pre_processing and split encoder function

--- pre_processing.py
+++ pre_processing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.impute import SimpleImputer as si
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 
@@ -14,20 +15,29 @@ class preprocess:
         df.reset_index(drop=True, inplace=True)
         return df
     
-    def encode(self, df_3):
-        integer_encoded = self.label_encoder.fit_transform(df_3)
+    def label_encode(self, original_df):
+        integer_encoded = self.label_encoder.fit_transform(original_df)
         integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+        return integer_encoded
+
+    def one_hot_encode(self, original_df):
+        integer_encoded = self.label_encode(original_df)
         onehot_encoded = self.onehot_encoder.fit_transform(integer_encoded)
         result = pd.DataFrame(onehot_encoded.toarray())
         return result
+    
+    def simple_impute(self, original_df: pd.DataFrame, column_name: list, strat="most_frequent") -> pd.DataFrame:
+        temp_df = original_df[column_name]
+        imputer = si(missing_values=np.nan, strategy=strat)
+        imputer.fit(temp_df)
+        original_df[column_name] = pd.DataFrame(imputer.transform(temp_df))
+        return original_df
 
     def main(self, df):
         df = self.remove_columns_not_required(df,["Cabin","Name"])
-        embarked_encoded = self.encode(df["Embarked"])
-        df[["em_1","em_2", "em_3"]] = embarked_encoded
+        df[["em_1","em_2", "em_3"]] = self.encode(df["Embarked"])
         df[["Sex_1","Sex_2"]] = self.encode(df["Sex"])
-        integer_encoded = self.label_encoder.fit_transform(df["Ticket"])
-        df["Ticket"] = integer_encoded.reshape(len(integer_encoded),1)
+        df["Ticket"] = self.label_encode(df["Ticket"])
         df.drop(columns=["Sex", "Embarked"], axis=1, inplace=True)
         return df
 
